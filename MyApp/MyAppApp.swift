8c3a6078c981926f144ebd14e01a6453b0e52da6
Niklas Wünsche <niklas.wuensche@tag24.de>
2025-07-15 10:55:06 +0200

    Build

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created on 15.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
